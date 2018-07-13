c40663ad420dcf98b88e49dcd6f729b28c7ad1fd
Ana Carolina Palante <aninhapalante@gmail.com>
2018-07-13 17:44:58 -0300

    Estrutura do projeto

---
+++ src/main/java/com/biotalk/biotalk/BiotalkApplication.java
@@ -0,0 +1,12 @@
+package com.biotalk.biotalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiotalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiotalkApplication.class, args);
+	}
+}
